89df77a3b59114efeb91794f4d0e4705b2bf3a00
Daniel Divino <danniel.valdivino.dv@gmail.com>
2025-12-11 21:04:38 -0400

    Novo Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cards } from './core/components/cards/cards';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Cards],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
